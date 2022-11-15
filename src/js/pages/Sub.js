ae93da03aff1deb9b287f7f3c8a7c6a437d98107
kawanishi <xoxo.nyanchuuu@gmail.com>
2022-11-16 00:33:21 +0900

    1115

--- src/js/pages/Sub.js
+++ src/js/pages/Sub.js
@@ -9,12 +9,34 @@ export default class Sub {
     $(document).on("mousemove",(e) => {
       const x = e.clientX
       const y = e.clientY
-
       cursor.css({
         "opacity": "1",
         "top": `${y}` + "px",
         "left": `${x}` + "px"
       })
-    })
+    });
+
+    // $('.fadeIn').each(function(i){
+    //     const WINDOW_HEIGHT = $(window).scrollTop();
+    //     const TARGET_HEIGHT = $(this).offset().top - 300;
+    //     if( WINDOW_HEIGHT > TARGET_HEIGHT){
+    //         $(this).addClass('in');
+    //         $('.fadeIn.in').each(function(i){
+    //             const delay = 100;
+    //             $(this).delay(i * delay).queue(function(next){
+    //                 $(this).addClass('show');
+    //                 next();
+    //             })
+    //         })
+    //     }
+    // });
+
+    $('.img-wrap img:nth-child(n+2)').hide();
+    setInterval(() => {
+      $(".img-wrap img:first-child").fadeOut(2000);
+      $(".img-wrap img:nth-child(2)").fadeIn(2000);
+      $(".img-wrap img:first-child").appendTo(".img-wrap");
+    }, 4500);
+
   }
 }
