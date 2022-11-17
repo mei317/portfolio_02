0ca0e55a0975fbfe7ca3ee8f0a75b2abc8e45a45
kawanishi <xoxo.nyanchuuu@gmail.com>
2022-11-18 00:27:13 +0900

    1117

--- src/js/index.js
+++ src/js/index.js
@@ -37,112 +37,114 @@ setInterval(() => {
 
 // profileフェードイン
 
-  // eslint-disable-next-line prefer-arrow-callback
-  $(window).on('scroll',function(){
-    let scroll_top = $(window).scrollTop();
-    $('.p-contentsProfile__text').each(function(){
-      let offset_top = $(this).offset().top,
-          top_margin = 700 ;
-      if( scroll_top > offset_top - top_margin ){
-          $(this).addClass('fadeIn');
-      }
-    });
+// eslint-disable-next-line prefer-arrow-callback
+$(window).on('scroll',function(){
+  let scroll_top = $(window).scrollTop();
+  $('.p-contentsProfile__text').each(function(){
+    let offset_top = $(this).offset().top,
+        top_margin = 600 ;
+    if( scroll_top > offset_top - top_margin ){
+        $(this).addClass('fadeIn');
+    }
   });
+});
 
-  // titleフェードイン
+// titleフェードイン
 
-  // eslint-disable-next-line prefer-arrow-callback
-  $(window).on('scroll',function(){
-    let scroll_top = $(window).scrollTop();
-    $('.c-contents__title').each(function(){
-      let offset_top = $(this).offset().top,
-          top_margin = 800 ;
-      if( scroll_top > offset_top - top_margin ){
-          $(this).addClass('fadeIn');
-      }
-    });
+// eslint-disable-next-line prefer-arrow-callback
+$(window).on('scroll',function(){
+  let scroll_top = $(window).scrollTop();
+  $('.c-content__title').each(function(){
+    let offset_top = $(this).offset().top,
+        top_margin = 800 ;
+    if( scroll_top > offset_top - top_margin ){
+        $(this).addClass('fadeIn');
+    }
   });
+});
 
-  // snsフェードイン
+// contentsTitle時間差フェードイン
 
-  // eslint-disable-next-line prefer-arrow-callback
-  $(window).on('scroll',function(){
-    let scroll_top = $(window).scrollTop();
-    $('.p-contentsSns__list li').each(function(){
-      let offset_top = $(this).offset().top,
-          top_margin = 700 ;
-      if( scroll_top > offset_top - top_margin ){
-          $(this).addClass('fadeIn');
-      }else{
-          $(this).removeClass('fadeIn');
-      }
-    });
+// eslint-disable-next-line prefer-arrow-callback
+$(window).on('scroll',function(){
+  let scroll_top = $(window).scrollTop();
+  $('.c-content__titleContact').each(function(){
+    let offset_top = $(this).offset().top,
+        top_margin = 650;
+    if( scroll_top > offset_top - top_margin ){
+        $(this).addClass('fadeIn');
+    }else{
+        $(this).removeClass('fadeIn');
+    }
   });
+});
 
-    // newsフェードイン
 
-  // eslint-disable-next-line prefer-arrow-callback
-  $(window).on('scroll',function(){
-    let scroll_top = $(window).scrollTop();
-    $('.p-contentsNews__item').each(function(){
-      let offset_top = $(this).offset().top,
-          top_margin = 700 ;
-      if( scroll_top > offset_top - top_margin ){
-          $(this).addClass('fadeIn');
-      }else{
-          $(this).removeClass('fadeIn');
-      }
-    });
+// snsフェードイン
+
+// eslint-disable-next-line prefer-arrow-callback
+$(window).on('scroll',function(){
+  let scroll_top = $(window).scrollTop();
+  $('.p-contentsSns__list li').each(function(){
+    let offset_top = $(this).offset().top,
+        top_margin = 700 ;
+    if( scroll_top > offset_top - top_margin ){
+        $(this).addClass('fadeIn');
+    }else{
+        $(this).removeClass('fadeIn');
+    }
   });
+});
 
+// newsフェードイン
 
-    // historyフェードイン
-
-  // eslint-disable-next-line prefer-arrow-callback
-  $(window).on('scroll',function(){
-    let scroll_top = $(window).scrollTop();
-    $('.p-contentsHistory__item a img').each(function(){
-      let offset_top = $(this).offset().top,
-          top_margin = 650 ;
-      if( scroll_top > offset_top - top_margin ){
-          $(this).addClass('fadeIn');
-      }else{
-          $(this).removeClass('fadeIn');
-      }
-    });
+// eslint-disable-next-line prefer-arrow-callback
+$(window).on('scroll',function(){
+  let scroll_top = $(window).scrollTop();
+  $('.p-contentsNews__item').each(function(){
+    let offset_top = $(this).offset().top,
+        top_margin = 700 ;
+    if( scroll_top > offset_top - top_margin ){
+        $(this).addClass('fadeIn');
+    }else{
+        $(this).removeClass('fadeIn');
+    }
   });
+});
 
-  // history時間差フェードイン
 
-  // eslint-disable-next-line prefer-arrow-callback
-  // $(window).on('scroll',function(){
-  //   let scroll_top = $(window).scrollTop();
-  //   $('.u-lastHistory a img').each(function(){
-  //     let offset_top = $(this).offset().top,
-  //         top_margin = 200 ;
-  //     if( scroll_top > offset_top - top_margin ){
-  //         $(this).addClass('fadeIn');
-  //     }else{
-  //         $(this).removeClass('fadeIn');
-  //     }
-  //   });
-  // });
+// historyフェードイン
 
-  //contentsTitle時間差フェードイン
+// eslint-disable-next-line prefer-arrow-callback
+$(window).on('scroll',function(){
+  let scroll_top = $(window).scrollTop();
+  $('.p-contentsHistory__list li').each(function(){
+    let offset_top = $(this).offset().top,
+        top_margin = 650 ;
+    if( scroll_top > offset_top - top_margin ){
+        $(this).addClass('fadeIn');
+    }else{
+        $(this).removeClass('fadeIn');
+    }
+  });
+});
 
-  // eslint-disable-next-line prefer-arrow-callback
-  // $(window).on('scroll',function(){
-  //   let scroll_top = $(window).scrollTop();
-  //   $('.c-contactTitle').each(function(){
-  //     let offset_top = $(this).offset().top,
-  //         top_margin = 500;
-  //     if( scroll_top > offset_top - top_margin ){
-  //         $(this).addClass('fadeIn');
-  //     }else{
-  //         $(this).removeClass('fadeIn');
-  //     }
-  //   });
-  // });
+// history時間差フェードイン
+
+// eslint-disable-next-line prefer-arrow-callback
+$(window).on('scroll',function(){
+  let scroll_top = $(window).scrollTop();
+  $('.p-contentsHistory__list li:last-child').each(function(){
+    let offset_top = $(this).offset().top,
+        top_margin = 450 ;
+    if( scroll_top > offset_top - top_margin ){
+        $(this).addClass('fadeIn');
+    }else{
+        $(this).removeClass('fadeIn');
+    }
+  });
+});
+
 
 
 
